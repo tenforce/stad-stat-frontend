21896353f193ba946f55b956502a5bb6b16cea2b
Aad Versteden <madnificent@gmail.com>
2017-02-10 00:49:14 +0100

    Calculate matching values in search route
    We calculate all matching values, based on the selected topics, in the
    search route.  This allows our state to be passed on correctly, and
    will be the basis for sharing the correct views with the other
    components.

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    topics: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    if( params.topics && params.topics.length ){
+      const joinedTopics = params.topics.join(',');
+      return this.get('store').query( 'value', { "filter[topics][id]": joinedTopics } );
+    } else {
+      return this.get('store').findAll( 'value' );
+    }
+  }
 });
