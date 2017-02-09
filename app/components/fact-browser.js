2445e38787ca9f4057ca018b990eb4a273312206
Karel Kremer <karel.kremer@gmail.com>
2017-02-09 17:56:36 +0100

    animate fake facts on click

--- app/components/fact-browser.js
+++ app/components/fact-browser.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  facts: [ "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "bar"]
+  facts: [ "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "foo", "bar"],
+
+  actions: {
+    clickFact: function() {
+      var count = Math.random()*20;
+      var facts = [];
+      for(var i=0; i<count; i++){
+        facts.push("fact"+i);
+      }
+      this.set('facts', facts);
+    }
+  }
 });
