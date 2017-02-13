3bf146a7ba00994f9b8116c4dca951b7b56ac0ba
Karel Kremer <karel.kremer@gmail.com>
2017-02-13 15:56:33 +0100

    charts in news

--- app/components/default-line-chart.js
+++ app/components/default-line-chart.js
@@ -5,12 +5,12 @@ export default Ember.Component.extend({
   classNames: ['line-chart chart'],
 
   title: "Test line chart",
-  categories: ["Jan", "Feb", "Dec"],
+  categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
   yTitle: "Test y line title",
   chartData:
     [
       {
-        name: 'Tokyo',
+        name: 'Tokyoxo',
         data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
       },
       {
