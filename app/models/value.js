6b9e242289bea75e49ac6a0aa5f413388460d3a6
langens-jonathan <flowofcontrol@gmail.com>
2017-02-14 15:37:45 +0100

    Added year to value model

--- app/models/value.js
+++ app/models/value.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  value: DS.attr('number'),
+    value: DS.attr('number'),
+    year: DS.attr(),
   regionTheme: DS.belongsTo('region-theme', { inverse: 'values' })
 });
