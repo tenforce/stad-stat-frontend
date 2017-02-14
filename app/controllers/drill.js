d1914eda8c4d4ea640c157521312c50b612f177d
Karel Kremer <karel.kremer@gmail.com>
2017-02-14 17:28:02 +0100

    chart scrolling

--- app/controllers/drill.js
+++ app/controllers/drill.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   leftOffset: 0,
-  chartSize: 600,
+  chartSize: 400,
   additionalTopics: [
     {
       title: "Verdeling inkomen"
