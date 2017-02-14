1b56be142742cce0c099fb318e997c4c71d009ac
Karel Kremer <karel.kremer@gmail.com>
2017-02-14 10:41:17 +0100

    link to for drill route using id so model reloads

--- app/routes/drill.js
+++ app/routes/drill.js
@@ -2,8 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    //return this.get('store').find('value',params.id);
-    return { topics: [ "foo", "bar", "bang"],
-             related:[1,2,3,4,5] };
+    return {
+      topics: [ "foo", "bar", "bang"],
+      related:[1,2,3,4,5]
+    };
   }
+
 });
