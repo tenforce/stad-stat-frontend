04e3e14e9f3ee6b18a25267e341807bb1f14da83
Karel Kremer <karel.kremer@gmail.com>
2017-02-14 10:40:49 +0100

    fix scrolling of charts

--- app/controllers/drill.js
+++ app/controllers/drill.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   leftOffset: 0,
-  chartSize: 250,
+  chartSize: 500,
   actions: {
     rightClick: function(){
-      var relatedSize = (this.get('model.related.length') ) * (this.get('chartSize'));
+      var relatedSize = (this.get('model.related.length') - 1) * (this.get('chartSize'));
       this.set('leftOffset', Math.max(-relatedSize, this.get('leftOffset') - this.get('chartSize')/2));
     },
     leftClick: function(){
