bab7caed8da2f7632d7451c1cab706de836fd9d2
Aad Versteden <madnificent@gmail.com>
2017-02-14 07:18:02 +0100

    Render pie-chart with real values in search
    The search now includes the pie chart with real values.  These values
    come from the backend.

--- app/components/default-pie-chart.js
+++ app/components/default-pie-chart.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   classNames: ['pie-chart chart'],
 
   title: "Test pie chart",
-  chartOptions: Ember.computed(function() {
+  chartOptions: Ember.computed('regionTheme.title', function() {
     return {
       chart: {
         plotBackgroundColor: null,
@@ -13,19 +13,29 @@ export default Ember.Component.extend({
         plotShadow: false
       },
       title: {
-        text: this.get('title')
+        text: this.get('regionTheme.title')
       },
       tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
       }
     };
   }),
-  chartData: Ember.computed(function() {
-    return [{
-      type: 'pie',
-      name: this.get('title'),
-      data: [["≤4", 1], ["5-13", 2], ["14-17", 3], ["18-24", 4], ["25-44", 5], ["45-64", 6], ["≥65", 7]]
-    }];
+  chartData: Ember.computed('regionTheme.values.@each.value', 'regionTheme.values.@each.year', 'regionTheme.title', function() {
+    if( this.get('regionTheme') && this.get('regionTheme.values') ){
+      return [{
+        type: 'pie',
+        name: this.get('regionTheme.title'),
+        data: this.get('regionTheme.values').map(
+          (value) => [value.get('year'), value.get('value')]
+        )
+      }];
+    } else {
+      return [{
+        type: 'pie',
+        name: this.get('title'),
+        data: [["≤4", 1], ["5-13", 2], ["14-17", 3], ["18-24", 4], ["25-44", 5], ["45-64", 6], ["≥65", 7]]
+      }];
+    }
   }),
   computedChartData: Ember.computed(function() {
     return [
